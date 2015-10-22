d1b01cb5f2d4b5813bae67022d6ffbe53aaf5da9
ptvan <pvan@fhcrc.org>
2015-10-22 09:12:09 -0700

    updated to plot using correct variable

--- tsne_functions.R
+++ tsne_functions.R
@@ -5,22 +5,20 @@ require(RColorBrewer)
 # plots the output of a tsne row on ICS flow data, highlights user-specified window, and return 
 # cell events within that window
 
-extract_tsne_region <- function(dat, x_min, x_max, y_min, y_max, stim, marker, facet) {
-  mrkr <- as.symbol(marker)
+extract_tsne_region <- function(dat, x_min, x_max, y_min, y_max, antigen, facet) {
   fct <- as.symbol(facet)
-  tmp <- subset(dat, stim==stim&x>x_min&x<x_max&y>y_min&y<y_max)
-  plotCall <- substitute(ggplot(dat, aes(x = x, y = y)) +
-                           geom_point(aes(colour = cl), size = 1.5, alpha = 0.5) +
+  tmp <- subset(dat, stim==antigen&x>x_min&x<x_max&y>y_min&y<y_max)
+  plotCall <- substitute(ggplot(subset(dat, stim==antigen), aes(x = x, y = y)) + 
+                           geom_point(aes(colour = degree), size = 1.5, alpha = 0.8) +
+                           geom_rect(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, 
+                                     color="red",alpha=0) +
                            facet_grid(fg ~ .) + 
-                           scale_color_gradientn(colours = rev(brewer.pal(11, "Spectral"))) +
-                           theme_bw() +
-                           geom_rect(xmin=x_min,xmax=x_max,ymin=y_min,ymax=y_max, color="red", alpha=0) 
+                           scale_color_gradientn(colours = rev(brewer.pal(11, "Spectral")))
                          ,
-                         list(cl = mrkr, fg = fct)
+                         list(fg = fct)
   )
   p <- eval(plotCall)
   
   print(p)
   return(tmp)
 }
-
