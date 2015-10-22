72e629dd1b5051e718dab42ec2488c3574745489
ptvan <pvan@fhcrc.org>
2015-10-22 14:01:05 -0700

    added function to run DBSCAN on tSNE output

--- tsne_functions.R
+++ tsne_functions.R
@@ -1,13 +1,18 @@
-require(Rtsne)
-require(ggplot2)
-require(RColorBrewer)
+library(Rtsne)
+library(ggplot2)
+library(RColorBrewer)
+library(gridExtra)
+library(dbscan)
 
 # plots the output of a tsne row on ICS flow data, highlights user-specified window, and return 
 # cell events within that window
 
 extract_tsne_region <- function(dat, x_min, x_max, y_min, y_max, antigen, facet) {
   fct <- as.symbol(facet)
-  tmp <- subset(dat, stim==antigen&x>x_min&x<x_max&y>y_min&y<y_max)
+  
+  dat <- subset(dat, stim==antigen)
+  tmp <- subset(dat, x>x_min&x<x_max&y>y_min&y<y_max)
+  
   plotCall <- substitute(ggplot(subset(dat, stim==antigen), aes(x = x, y = y)) + 
                            geom_point(aes(colour = degree), size = 1.5, alpha = 0.8) +
                            geom_rect(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, 
@@ -18,7 +23,12 @@ extract_tsne_region <- function(dat, x_min, x_max, y_min, y_max, antigen, facet)
                          list(fg = fct)
   )
   p <- eval(plotCall)
-  
   print(p)
   return(tmp)
+}
+
+cluster_with_dbscan <- function(dat, epsilon, minpts = 5 ) {
+  out <- dbscan(dat[,.(x,y)], eps = epsilon)
+  dat[,"dbscan_cluster"] <- out$cluster
+  return(dat)
 }
