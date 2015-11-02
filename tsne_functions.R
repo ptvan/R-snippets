1a071e3370705c55d85e6f8d183f922af9b10fc6
ptvan <pvan@fhcrc.org>
2015-11-02 10:00:51 -0800

    minor cosmetic changes

--- tsne_functions.R
+++ tsne_functions.R
@@ -4,22 +4,25 @@ library(RColorBrewer)
 library(gridExtra)
 library(dbscan)
 library(sp)
+library(pheatmap)
 
 # plots the output of a tsne row on ICS flow data, highlights user-specified window, and return 
 # cell events within that window
 
 extract_tsne_rect <- function(dat, x_min, x_max, y_min, y_max, antigen, facet) {
   fct <- as.symbol(facet)
-  
+
   dat <- subset(dat, stim==antigen)
   tmp <- subset(dat, x>x_min&x<x_max&y>y_min&y<y_max)
   
   plotCall <- substitute(ggplot(subset(dat, stim==antigen), aes(x = x, y = y)) + 
-                           geom_point(aes(colour = degree), size = 1.5, alpha = 0.8) +
+                           geom_point(aes(colour = degree), size = 1, alpha = 0.8) +
                            geom_rect(xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, 
                                      color="red",alpha=0) +
+                           labs(title = "tSNE comparison") +
                            facet_grid(fg ~ .) + 
-                           scale_color_gradientn(colours = rev(brewer.pal(11, "Spectral")))
+                           scale_color_gradientn(colours = rev(brewer.pal(11, "Spectral"))) 
+                           
                          ,
                          list(fg = fct)
   )
@@ -34,4 +37,3 @@ cluster_with_dbscan <- function(dat, epsilon, minpts = 5 ) {
   dat[,"dbscan_cluster"] <- out$cluster
   return(dat)
 }
-
