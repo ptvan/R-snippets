6a242238504626bf3ce1e288f4cbb055d871b6b3
ptvan <pvan@fhcrc.org>
2015-11-19 15:22:29 -0800

    cleaned up code

--- tsne_functions.R
+++ tsne_functions.R
@@ -7,17 +7,15 @@ library(sp)
 library(pheatmap)
 
 # plots the output of a tsne run on ICS flow data, highlights user-specified window, and return 
-# cell events within that window. Also calls tsne_proportion_boxplot() to perform t.test() on
-# extracted events
+# cell events within that window. Also perform t.test() on extracted events
 
 extract_tsne_rect <- function(dat, x_min, x_max, y_min, y_max, antigen, marker, facet, title) {
   facet <- as.symbol(facet)
   marker <- as.symbol(marker)
   
   # `marker` argument can be an actual marker (eg. "IFNg") or "degree" 
-  
   dat <- subset(dat, stim==antigen)
-  tmp <- subset(dat, x>x_min&x<x_max&y>y_min&y<y_max)
+  
   
   plotCall <- substitute(ggplot(dat, aes(x = x, y = y)) + 
                            geom_point(aes(colour = mkr), size = 1, alpha = 0.8) +
@@ -31,20 +29,13 @@ extract_tsne_rect <- function(dat, x_min, x_max, y_min, y_max, antigen, marker, 
   )
   
   p1 <- eval(plotCall)
-  p2 <- tsne_proportion_boxplots(dat, x_min, x_max, y_min, y_max, antigen, marker, facet)
-  p3 <- grid.rect(gp=gpar(col="white"))
-  pl <- grid.arrange(p1, arrangeGrob(p2,p3, nrow=1), heights=c(1,0.5))
-  print(pl)
-  return(tmp)
-}
-
-# performs a t.test() on extracted events
-tsne_proportion_boxplots <- function(dat, x_min, x_max, y_min, y_max, antigen, marker, facet) {
+  
   m <- unique(dat[,.(ptid,neut_status,controller_status)])
+  tmp <- subset(dat, x>x_min&x<x_max&y>y_min&y<y_max)
+  
   totals <- dat[, length(x), by=ptid]
   setnames(totals, "V1", "total")
   
-  dat <- subset(dat, stim==antigen)
   dat <- subset(dat, x>x_min&x<x_max&y>y_min&y<y_max)
   
   non_h <- subset(dat, neut_status =="non")
@@ -64,17 +55,18 @@ tsne_proportion_boxplots <- function(dat, x_min, x_max, y_min, y_max, antigen, m
   
   p <- t.test(log_proportion ~ neut_status, data=m)
   
-  pl <- ggplot(m, aes(x=neut_status, y=log(in_hull))) + 
+  p2 <- ggplot(m, aes(x=neut_status, y=log(in_hull))) + 
     geom_boxplot(outlier.size = 3) +
     geom_point(aes(col=ptid), size=3) +
     theme(legend.position="none") +
     labs(title=paste0("Selected cell proportion, p=", round(p$p.value, 3)), x="") 
   
+  p3 <- grid.rect(gp=gpar(col="white"))
+  pl <- grid.arrange(p1, arrangeGrob(p2,p3, nrow=1), heights=c(1,0.5))
+  print(pl)
   print(m)
-  return(pl)
+  return(tmp)
 }
-
-
 
 
 cluster_with_dbscan <- function(dat, epsilon, minpts = 5 ) {
