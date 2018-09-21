a38edf5ade40c36db83172d56581c29964d63ec3
ptvan <pvan@fhcrc.org>
2018-09-21 11:31:54 -0700

    added comments and example usage to geneExpr_functions.R

--- geneExpr_functions.R
+++ geneExpr_functions.R
@@ -1,43 +1,51 @@
 summarizeGenelist <- function (dat, geneList, grp1name="group1", grp2name="group2", grp1idx, grp2idx, plot=FALSE){
   # compares the mean expression between two groups of genes in a given 
-  # expression matrix
-  
-  # optionally makes boxplots of the comparison, faceted by gene
+  # expression matrix, optionally makes boxplots of the comparison, faceted by gene
   # used in a pipeline, so checks are pretty rudimentary
   
+  # example: out <- summarizeGenelist(expressionMatrix,
+  #                                   c("LAP3","NCOA7","IFIT2","STAT1","TRIM21"), 
+  #                                   grp1name = "control", 
+  #                                   grp2name = "experimental", 
+  #                                   which(grepl("control", colnames(expressionMatrix))), 
+  #                                   which(grepl("experimental", colnames(expressionMatrix)))  
+  #                                   )
+
   require(ggplot2)
   require(data.table)
+  
+  # check that at least some of data has genes in the specified geneList
   if (length(intersect(geneList, rownames(dat))) > 0){
       
     dat <- subset(dat, rownames(dat) %in% geneList)
-      
+      # check that all columns have group assignments and the groups don't overlap
       if (length(grp1idx) + length(grp2idx) == ncol(dat) && !identical(grp1idx, grp2idx)){
-      rows <- rownames(dat)
-      cols <- colnames(dat)
-      idxs <- cols
-      idxs[grp1idx] <- grp1name
-      idxs[grp2idx] <- grp2name
-      idx <- data.frame(cbind(cols,idxs))
-      d <- data.table(merge(melt(dat), idx, by.x="Var2", by.y="cols"))
-      setnames(d, c("Var1","Var2","value", "idxs"), c("geneName", "inputColumn","expr","group"))
-      
-      tab <- merge(subset(d, group==grp1name)[,list(group1expr=mean(expr)),by=geneName],
-                   subset(d, group==grp2name)[,list(group2expr=mean(expr)),by=geneName],
-                   by="geneName")
-      setnames(tab, c("group1expr", "group2expr"), c(grp1name,grp2name))
-      tab$higher <- tab[,..grp2name] > tab[,..grp1name]
-      tab$higher <- gsub("FALSE", grp1name, tab$higher)
-      tab$higher <- gsub("TRUE", grp2name, tab$higher)
-      
-      if(plot) {
-        ggplot(d, aes(x=group, y=expr, col=group)) +
-          geom_boxplot(outlier.shape = NA) +
-          facet_wrap(~ geneName, scales="free") +
-          theme(legend.position = "none") +
-          labs(title=paste0(length(rows), " genes in list"))
-      }
-      return(tab)
-      
+        rows <- rownames(dat)
+        cols <- colnames(dat)
+        idxs <- cols
+        idxs[grp1idx] <- grp1name
+        idxs[grp2idx] <- grp2name
+        idx <- data.frame(cbind(cols,idxs))
+        d <- data.table(merge(melt(dat), idx, by.x="Var2", by.y="cols"))
+        setnames(d, c("Var1","Var2","value", "idxs"), c("geneName", "inputColumn","expr","group"))
+        
+        tab <- merge(subset(d, group==grp1name)[,list(group1expr=mean(expr)),by=geneName],
+                     subset(d, group==grp2name)[,list(group2expr=mean(expr)),by=geneName],
+                     by="geneName")
+        setnames(tab, c("group1expr", "group2expr"), c(grp1name,grp2name))
+        tab$higher <- tab[,..grp2name] > tab[,..grp1name]
+        tab$higher <- gsub("FALSE", grp1name, tab$higher)
+        tab$higher <- gsub("TRUE", grp2name, tab$higher)
+        
+        if(plot) {
+          ggplot(d, aes(x=group, y=expr, col=group)) +
+            geom_boxplot(outlier.shape = NA) +
+            facet_wrap(~ geneName, scales="free") +
+            theme(legend.position = "none") +
+            labs(title=paste0(length(rows), " genes in list"))
+        }
+        return(tab)
+        
     } else { stop ("must specify one unique index for each column of data !!!") }
     
     colnames(d) <- c("gene", "group", "expression")
