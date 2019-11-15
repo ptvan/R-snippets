63c858770290459039a54ec9bb0832a8fa50767f
Phu T. Van <pvan@fredhutch.org>
2019-11-15 12:18:46 -0800

    updated timeseries_analysis.R

--- timeseries_analysis.R
+++ timeseries_analysis.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(zoo)
 library(forecast)
 
-# import data
+### STEPS DATA
 steps <- read.csv("stepsData.csv")
 
 # clean things up
@@ -15,13 +15,11 @@ steps <- read.csv("stepsData.csv")
 
 # day-level data
 steps <- steps %>%
-  mutate(creationDate = as.Date(creationDate)) %>%
   mutate(startDate = as.Date(startDate)) %>%
-  mutate(endDate = as.Date(endDate)) %>%
   group_by(startDate) %>%
   summarize(stepsWalked = sum(stepsWalked))
 
-# create a ts
+# create a ts object
 steps_ts <- ts(steps$stepsWalked, start = c(2015,yday(steps$startDate[1])), frequency = 365)
 
 # find a weekly trend
@@ -46,3 +44,21 @@ par(mfrow=c(3,1))
 plot(steps_stl_seasonal, main="seasonal component")
 plot(steps_stl_trend, main="trend component")
 plot(steps_stl_random, main="random component")
+
+
+### BIKING DISTANCE DATA
+biking <- read.csv("cyclingData.csv", header=T)
+biking <- biking %>%
+  mutate(startDate = as.Date(startDate)) %>%
+  group_by(startDate) %>%
+  summarize(kcalBurned = sum(kcalBurned),milesCycled = sum(milesCycled)) %>%
+  mutate(startDate = as.character(startDate)) 
+
+# fill in missing dates & merge
+missing_dates <- as.character(seq(as.Date(biking$startDate[1]), as.Date(biking$startDate[279]), by=1)) %>%
+  setdiff(.,  biking$startDate) %>%
+  data.frame()
+colnames(missing_dates) <- c("startDate")
+missing_dates$kcalBurned = 0
+missing_dates$milesCycled = 0  
+biking <- rbind(biking, missing_dates) %>% read.zoo(format="%Y-%m-%d")
