58855e177ed82dfa8973d911a1dd51fcf64b7b8f
Phu T. Van <pvan@fredhutch.org>
2019-11-18 12:42:44 -0800

    added comments to modeling_gaussian_mixtures.R

--- modeling_gaussian_mixtures.R
+++ modeling_gaussian_mixtures.R
@@ -13,12 +13,16 @@ steps <- steps %>%
   group_by(startDate) %>%
   summarize(stepsWalked = sum(stepsWalked))
 
+# do density estimation, let mclust pick our distribution count for us
 dens <- densityMclust(steps$stepsWalked)
+
+# look at dianogstics
 plot(dens, what = "BIC")
 summary(dens$BIC)
 summary(dens, parameters = TRUE)
 plot(dens, what = "density", data=steps$stepsWalked)
 
+# extract parameters of the Gaussians
 params <- dens$parameters
 nDistros <- length(params$pro)
 
@@ -26,16 +30,17 @@ distros <- data.frame(matrix(0,4,4))
 colnames(distros) <- c("id","n", "mean","sd")
 distros$id <- 1:4
 
+# re-sample the Gaussians
 N <- nrow(steps)
 for (i in 1:nDistros){
   distros[distros$id==i,]$n <- floor(N * params$pro[i])
   distros[distros$id==i,]$mean <- params$mean[i]
   distros[distros$id==i,]$sd <- floor(sqrt(params$variance$sigmasq[i]))
 }
-
+set.seed(1)
 densities <- apply(distros[, -1], 1, function(x) rnorm(n = x[1], mean = x[2], sd = x[2]))
 
-
+# plot out the estimated densities
 plot(0, type = "n", xlim = c(min(unlist(lapply(densities, min))),max(unlist(lapply(densities, max)))), ylim = c(0, 1.5e-4))
 for (d in 1:length(densities)){
   lines(density(densities[[d]]), lty = d)
