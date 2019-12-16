3a4ce55a7108bf70b42512795ebe19a3af766fd6
Phu T. Van <pvan@fredhutch.org>
2019-12-16 10:38:33 -0800

    updated imbalanced_data_analysis.R

--- imbalanced_data_analysis.R
+++ imbalanced_data_analysis.R
@@ -1,8 +1,19 @@
 library(smotefamily)
 library(ROSE)
+library(dplyr)
 
+# data from the ROSE package
 data(hacide)
 
-dat_plot = SMOTE(hacide.train[,2:3],
-                 as.numeric(hacide.train$cls),
-                 K = 3, dup_size = 0)
+# data imbalance: minority outcome only occurs in 2% of cases
+hacide.train %>% group_by(cls) %>% count()
+
+# SMOTE
+smote_out <- SMOTE(hacide.train[,2:3],
+             as.numeric(hacide.train$cls),
+             K = 3, dup_size = 0)
+
+# ADASYN
+adasyn_out <- ADAS(hacide.train[,2:3],
+      as.numeric(hacide.train$cls),
+      K = 3)
