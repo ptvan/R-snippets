dfafabccde9358cd71d0adbe93cd879cab45b3e8
ptvan <pvan@fredhutch.org>
2020-02-13 08:11:57 -0800

    reorg & added methylation_workflow.R

--- Pmisc.R
+++
@@ -1,9 +0,0 @@
-extract_lm_pval <- function (model){
-# extracts F-statistic from a linear model object
-# then calculates P-value
-	if ( class(model) != "lm") stop("Not an object of class 'lm' ")
-	f <- summary(model)$fstatistic
-	p <- pf(f[1],f[2],f[3],lower.tail=F)
-	attributes(p) <- NULL
-	return(p)
-}
