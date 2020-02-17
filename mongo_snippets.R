aca0ebc2e321384a1d16df22d209ff5e22610ab7
ptvan <pvan@fredhutch.org>
2020-02-17 07:39:45 -0800

    updated mongo_snippets.R

--- mongo_snippets.R
+++ mongo_snippets.R
@@ -1,3 +1,20 @@
 library(mongolite)
+library(dplyr)
+library(magrittr)
+
 m <- mongo(collection = "test", db = "test", url = "mongodb://localhost",
            verbose = FALSE, options = ssl_options())
+
+steps <- read.csv("~/working/datasets/iphone_health/stepsData.csv")
+
+# day-level data
+steps <- steps %>%
+  mutate(startDate = as.Date(startDate)) %>%
+  group_by(startDate) %>%
+  summarize(stepsWalked = sum(stepsWalked))
+
+iphone_steps <- mongo("steps")
+iphone_steps$insert(steps)
+iphone_steps$count()
+
+print(iphone_steps$find('{}'))
