e79df14b91a0cfd732ae27c3dcc3b24734d98c3d
Phu T. Van <pvan@fredhutch.org>
2020-02-28 12:03:16 -0800

    consolidated classification & regression code

--- classificationregression_snippets.R
+++ classificationregression_snippets.R
@@ -54,4 +54,43 @@ ldahist(plda$x[,2], g = plda$class)
 # ggplot is a little nicer...
 irisProjection <- cbind(scale(as.matrix(iris[,-5]),scale=FALSE) %*% r$scaling,iris[,5,drop=FALSE])
 ggplot(data=irisProjection,aes(x=LD1,y=LD2,col=Species)) +
- geom_point()   
+ geom_point()  
+
+##########################
+# XGBOOST
+##########################
+
+library(xgboost)
+
+data(agaricus.train, package='xgboost')
+data(agaricus.test, package='xgboost')
+train <- agaricus.train
+test <- agaricus.test
+
+# random forests
+bst_rf <- xgboost(data = train$data
+                  , label = train$label
+                  , max_depth = 4
+                  , num_parallel_tree = 1000
+                  , subsample = 0.5
+                  , colsample_bytree =0.5
+                  , nrounds = 1
+                  , objective = "binary:logistic")
+
+bst <- xgboost(data = dtrain
+               , booster = "gbtree"
+               , max_depth = 2
+               , eta = 1
+               , nthread = 2
+               , nrounds = 2
+               , objective = "binary:logistic", verbose = 2)
+
+# XGB's default is regression, so the predictions are probabilities
+pred <- predict(bst, test$data)
+
+prediction <- as.numeric(pred > 0.5)
+err <- mean(prediction != test$label)
+print(paste("test-error=", err))
+
+xgb.importance(model = bst)
+xgb.plot.shap(agaricus.test$data, model = bst, features = "odor=none")
