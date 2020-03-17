6cf2f63fd11f7ecc4dc2c2794adadc7b0500f6a3
ptvan <pvan@fredhutch.org>
2020-03-17 13:39:04 -0700

    added reference

--- dimensionreduction_snippets.R
+++ dimensionreduction_snippets.R
@@ -1,7 +1,12 @@
 library(uwot)
 library(Rtsne)
 library(kernlab)
-library(keras)
+library(keras) # for autoencoders
+library(MASS) # for Sammon mapping
+library(diffusionMap)
+
+# reference: Dimensionality Reduction: A Comparative Review, van der Maaten et al 2009 
+# https://lvdmaaten.github.io/publications/
 
 
 
