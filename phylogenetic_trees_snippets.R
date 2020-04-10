76f0f6b09e8606de73aab50c764f4a128c3d78e0
ptvan <pvan@fredhutch.org>
2020-04-10 09:53:15 -0700

    added notes about phylo vs. tidytree classes

--- phylogenetic_trees_snippets.R
+++ phylogenetic_trees_snippets.R
@@ -8,37 +8,52 @@ library(stringr)
 setwd("~/working/workflows/")
 
 ## read in the aligned sequences
+# and create metadata
 COVIDseqs <- readDNAStringSet("COVID19multi.phy")
 meta <- data.frame(matrix(unlist(str_split(names(COVIDseqs), pattern = "\\|")), nrow=length(COVIDseqs), byrow=TRUE))
+colnames(meta) <- c("genBank", "date", "location")
 names(COVIDseqs) <- gsub("\\|","_", names(COVIDseqs))
+meta$label <- names(COVIDseqs)
+
+# write the aligned sequences out as a FASTA
 write.dna(COVIDseqs, "COVID19MSA.fa", "fasta")
+
 ## calculate Hamming distance
 # tipseq_dist <- stringDist(COVIDseqs, method = "hamming")
 
+## read IQ-TREE output
+phylo <- read.newick("COVID19multi.phy.treefile")
 
-COVIDtree <- read.newick("COVID19multi.phy.treefile")
 ## drop a duplicate tip from the tree
-# COVIDtree <- drop.tip(COVIDtree, COVIDtree$tip.label[which(grepl("MT042773", mytree$tip.label))])
+# phylo <- drop.tip(phylo, phylo$tip.label[which(grepl("MT042773", mytree$tip.label))])
 
-## COVIDtree is a 'phylo' objects, which has some useful methods
-# plot(COVIDtree)
-# plot(COVIDtree, type="unrooted")
+## phylo is a S3 'phylo' objects, which has some useful methods
+# plot(phylo)
+# plot(phylo, type="unrooted")
 # nodelabels()
 # tiplabels()
-# rerooted <- root(COVIDtree)
-# all.equal(rerooted, COVIDtree)
+# rerooted <- root(phylo)
+# all.equal(rerooted, phylo)
+
+## tidytree implements a S4 'treedata' object, allowing metadata
+# tidyTree <- full_join(as.treedata(phylo), meta, by="label")
 
 ## plotting using ggtree
-# ggtree(COVIDtree, layout="circular") +
-#   geom_tiplab(size=3, color="blue")
+# ggtree(tidyTree, layout="circular") +
+# geom_tiplab(size=3, color="blue")
 
-ggtree(COVIDtree) +
+## with timescale
+ggtree(tidyTree) +
   geom_tiplab() +
   theme_tree2() +
   xlim_tree(3) +
   geom_treescale(x=0, y=45, width=1, color='red')
 
+## with metadata
+ggtree(tidyTree, branch.length='none') +
+  geom_tiplab(aes(label=genBank))
+
 ## plot the tree along the multiple sequence alignment
-p <- ggtree(COVIDtree, branch.length='none') + 
+p <- ggtree(tidyTree, branch.length='none') + 
   geom_tiplab() + xlim_tree(5.5) 
 msaplot(p, "COVID19MSA.fa", offset=10)
