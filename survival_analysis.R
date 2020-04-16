4db4bdd42881a72827cf61d9337ae6a7e1b01691
ptvan <pvan@fredhutch.org>
2020-04-16 08:08:04 -0700

    added Cox example

--- survival_analysis.R
+++ survival_analysis.R
@@ -7,22 +7,31 @@ library(dplyr)
 data(veteran)
 head(veteran)
 
-## Kaplan-Meier Survival Curve
+#### Kaplan-Meier Survival Curve
 km <- with(veteran, Surv(time, status))
 
 km_fit <- survfit(Surv(time, status) ~ 1, data=veteran)
 summary(km_fit, times = c(1,30,60,90*(1:10)))
 
-
-## splitting subjects by age & rerunning the model
+# splitting subjects by age & rerunning the model
 vet <- mutate(veteran, AG = ifelse((age < 60), "LT60", "OV60"),
               AG = factor(AG),
               trt = factor(trt,labels=c("standard","test")),
               prior = factor(prior,labels=c("N0","Yes")))
 km_AG_fit <- survfit(Surv(time, status) ~ AG, data=vet)
 
+#### Cox Proportional Hazard model
+# fitting all covars
+# NOTE: Cox assumes that covariates do not vary with time
+cox <- coxph(Surv(time, status) ~ trt + celltype + 
+               karno + diagtime + age + prior 
+               , data = vet)
+summary(cox)
+cox_fit <- survfit(cox)
+autoplot(cox_fit)
 
-## Random-Forest survival using ranger on age-split data
+
+#### Random-Forest survival using ranger on age-split data
 r_fit <- ranger(Surv(time, status) ~ trt + celltype + 
                   karno + diagtime + age + prior,
                 data = vet,
