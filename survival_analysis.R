26316c745180db74ba437c38acd08415d934299d
ptvan <pvan@fredhutch.org>
2020-04-16 07:56:50 -0700

    added age stratification and ranger example

--- survival_analysis.R
+++ survival_analysis.R
@@ -3,13 +3,35 @@ library(ggfortify)
 library(ranger)
 library(dplyr)
 
-data(veteran)
-
-head(veteran)
 # note: score = Karnofsky performance score
+data(veteran)
+head(veteran)
 
 ## Kaplan-Meier Survival Curve
 km <- with(veteran, Surv(time, status))
 
 km_fit <- survfit(Surv(time, status) ~ 1, data=veteran)
 summary(km_fit, times = c(1,30,60,90*(1:10)))
+
+
+## splitting subjects by age & rerunning the model
+vet <- mutate(veteran, AG = ifelse((age < 60), "LT60", "OV60"),
+              AG = factor(AG),
+              trt = factor(trt,labels=c("standard","test")),
+              prior = factor(prior,labels=c("N0","Yes")))
+km_AG_fit <- survfit(Surv(time, status) ~ AG, data=vet)
+
+
+## Random-Forest survival using ranger on age-split data
+r_fit <- ranger(Surv(time, status) ~ trt + celltype + 
+                  karno + diagtime + age + prior,
+                data = vet,
+                mtry = 4,
+                importance = "permutation",
+                splitrule = "extratrees",
+                verbose = TRUE)
+
+# Average the survival models
+death_times <- r_fit$unique.death.times 
+surv_prob <- data.frame(r_fit$survival)
+avg_prob <- sapply(surv_prob,mean)
