994f66744f7d385de1a6adb4447e22fa5fa9152c
ptvan <pvan@fredhutch.org>
2020-04-21 07:10:30 -0700

    added basic plotting

--- meta-analysis.R
+++ meta-analysis.R
@@ -21,3 +21,19 @@ escalc(out ~ grp | study, weights = freq, data = dat.fm, measure = "RR")
 # fitting the model
 res <- rma(ai = tpos, bi = tneg, ci = cpos, di = cneg, data = dat,  measure = "RR")
 confint(res)
+
+# plot output using the package's forest()
+forest(res, slab = paste(dat$author, dat$year, sep = ", ")
+       , xlim = c(-16, 6)
+       , at = log(c(0.05, 0.25, 1, 4))
+       , atransf = exp
+       , ilab = cbind(dat$tpos, dat$tneg, dat$cpos, dat$cneg)
+       , ilab.xpos = c(-9.5, -8, -6, -4.5), cex = 0.75)
+
+op <- par(cex = 0.75, font = 2)
+text(c(-9.5, -8, -6, -4.5), 15, c("TB+", "TB-", "TB+", "TB-"))
+text(c(-8.75, -5.25), 16, c("Vaccinated", "Control"))
+text(-16, 15, "Author(s) and Year", pos = 4)
+text(6, 15, "Relative Risk [95% CI]", pos = 2)
+
+par(op)
