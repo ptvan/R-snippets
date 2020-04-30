8ff324eb164e70f25abe2bdedc5819d79e3d85b7
ptvan <pvan@fredhutch.org>
2020-04-30 08:46:22 -0700

    added mixOmics example

--- dataintegration_snippets.R
+++ dataintegration_snippets.R
@@ -1,6 +1,7 @@
 #########################
 # using the MOFA package
 #########################
+# NOTE: need to update below to MOFA v2
 library(MOFA)
 
 # simulate some data & create MOFA object
@@ -39,5 +40,45 @@ MOFAobject.trained <- runMOFA(MOFAobject, outfile)
 # using the mixOmics package
 ############################
 library(mixOmics)
+
+# load example data
+data(breast.TCGA)
+
+X <- list(mRNA = breast.TCGA$data.train$mrna,
+          miRNA = breast.TCGA$data.train$mirna,
+          protein = breast.TCGA$data.train$protein)
+Y <- breast.TCGA$data.train$subtype
+list.keepX <- list(mRNA = c(16, 17), miRNA = c(18,5), protein = c(5, 5))
+
+# run Projection to Latent Structure with sparse Discriminant Analysis
+MyResult.diablo <- block.splsda(X, Y, keepX=list.keepX)
+
+# plot individuals and variables
+plotIndiv(MyResult.diablo
+          ,ind.names = FALSE
+          ,legend=TRUE, cex=c(1,2,3)
+          # title = 'BRCA with DIABLO'
+          )
+
+plotVar(MyResult.diablo
+        , var.names = c(FALSE, FALSE, TRUE)
+        ,legend=TRUE, pch=c(16,16,1)
+        )
+
+# circos of correlation between data types
+circosPlot(MyResult.diablo
+           , line=TRUE
+           , cutoff=0.7)
+
+# heatmap of a single component
+cimDiablo(MyResult.diablo
+          , color.blocks = c('darkorchid', 'brown1', 'lightgreen')
+          , comp = 1
+          , margin=c(8,20)
+          , legend.position = "right")
+
+# loadings
+plotLoadings(MyResult.diablo
+             , comp = 1, contrib = "max")
 
 
