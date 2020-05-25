de0bc8b7ed9d71797524f33a7f9bd9276fdea079
ptvan <pvan@fredhutch.org>
2020-05-25 06:28:51 -0700

    initial commit

---
+++ simulation_snippets.R
@@ -0,0 +1,21 @@
+library(simmer)
+set.seed(999)
+
+env <- simmer("mySim")
+
+patient <- trajectory("patients' path") %>%
+seize("nurse", 1) %>%
+timeout(function() rnorm(1, 15)) %>%
+release("nurse", 1) %>%
+seize("doctor", 1) %>%
+timeout(function() rnorm(1, 20)) %>%
+release("doctor", 1) %>%
+seize("administration", 1) %>%
+timeout(function() rnorm(1, 5)) %>%
+release("administration", 1)
+
+env %>%
+add_resource("nurse", 1) %>%
+add_resource("doctor", 2) %>%
+add_resource("administration", 1) %>%
+add_generator("patient", patient, function() rnorm(1, 10, 2))
