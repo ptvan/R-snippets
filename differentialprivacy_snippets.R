6c19fce0d53057d2b2f3db6f47a8f669a04c6d69
ptvan <pvan@fredhutch.org>
2020-06-04 08:15:25 -0700

    updates

--- differentialprivacy_snippets.R
+++ differentialprivacy_snippets.R
@@ -5,3 +5,7 @@ mech <- DPMechLaplace(target = target)
 
 distr <- function(n) rnorm(n)
 mech <- sensitivitySampler(mech, oracle = distr, n = 5, gamma = 0.1)
+
+# length is sensitivitySampler() n
+X <- c(4.11,-0.1,-0.14, 1.516, 2.43) 
+r <- releaseResponse(mech, privacyParams = DPParamsEps(epsilon = 1), X = X)
