7856c94e5f9a3b8b34c2633755efed473b163725
ptvan <pvan@fredhutch.org>
2020-06-07 07:09:44 -0700

    updates

--- datavalidation_snippets.R
+++ datavalidation_snippets.R
@@ -2,3 +2,7 @@ library(validate)
 data(women)
 cf <- check_that(women, height > 0, weight > 0, height/weight > 0.5)
 summary(cf)
+
+rules <- validator(height == women_reference$height)
+cf <- confront(women, rules, ref = list(women_reference = women1))
+summary(cf)
