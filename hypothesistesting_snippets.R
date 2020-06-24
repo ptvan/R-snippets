e68c28312b69b692e8276d1469f73d8eaafaa169
ptvan <pvan@fredhutch.org>
2020-06-24 07:17:22 -0700

    updates

--- hypothesistesting_snippets.R
+++ hypothesistesting_snippets.R
@@ -1,3 +1,5 @@
+library(MASS)
+
 dat1 <- runif(1000)
 dat2 <- runif(1000)
 dat3 <- runif(1000)
@@ -10,3 +12,7 @@ df <- reshape2::melt(cbind(dat1, dat2, dat3))[,c("Var2","value")]
 colnames(df) <- c("group","value")
 anovaOut <- aov(value ~ group, data=df)
 summary(anovaOut)
+
+# Pearson's Chi-Squared
+tbl <- table(survey$Smoke, survey$Exer) 
+chisq.test(tbl) 
