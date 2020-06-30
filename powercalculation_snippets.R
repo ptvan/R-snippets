b1289303aabb6138718b198ad7cd635e8e5e25f9
ptvan <pvan@fredhutch.org>
2020-06-30 07:46:02 -0700

    updates

--- powercalculation_snippets.R
+++ powercalculation_snippets.R
@@ -5,3 +5,6 @@ pwr.t2n.test(n1 = 30, n2= 30, d = 0.2, sig.level =0.05)
 
 # correlation
 pwr.r.test(n = 30, r = 0.5, sig.level = 0.05) 
+
+# ANOVA
+pwr.anova.test(k = 3, n = 30, f = 0.1, sig.level = 0.05) 
