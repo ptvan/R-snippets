2c52a5ab463a8a31ec16459ccbcd70e2369d084a
ptvan <pvan@fredhutch.org>
2020-07-02 07:41:27 -0700

    updates

--- powercalculation_snippets.R
+++ powercalculation_snippets.R
@@ -7,4 +7,7 @@ pwr.t2n.test(n1 = 30, n2= 30, d = 0.2, sig.level =0.05)
 pwr.r.test(n = 30, r = 0.5, sig.level = 0.05) 
 
 # ANOVA
-pwr.anova.test(k = 3, n = 30, f = 0.1, sig.level = 0.05) 
+pwr.anova.test(k = 3, n = 30, f = 0.1, sig.level = 0.05)
+
+# Proportions
+pwr.2p.test(h = 0.2, n = 30, sig.level = 0.05) 
