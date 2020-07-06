ce5a34a89b2fd096f9be8c4158479c531d6d04a3
ptvan <pvan@fredhutch.org>
2020-07-06 07:20:56 -0700

    initial commit

--- linearmodels_snippets.R
+++ linearmodels_snippets.R
@@ -1,5 +1,11 @@
 library(glmm)
 library(lme4)
 
-data("Dyestuff")
-fm1 <- lmer(Yield ~ 1 + (1 | Batch), Dyestuff)
+data(Dyestuff)
+# using restricted maximum likelihood
+fm1 <- lmer(Yield ~ 1 + (1 | Batch), data = Dyestuff, REML=FALSE)
+summary(fm1)
+
+# using log-likelihood
+fm2 <- lmer(Yield ~ 1 + (1 | Batch), data = Dyestuff, REML=TRUE)
+summary(fm2)
