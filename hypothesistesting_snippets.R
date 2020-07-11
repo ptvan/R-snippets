02ebb499710811380e8ea1877aba4b1f21692ba5
ptvan <pvan@fredhutch.org>
2020-07-11 09:10:40 -0700

    updated

--- hypothesistesting_snippets.R
+++ hypothesistesting_snippets.R
@@ -6,33 +6,37 @@ dat1 <- runif(1000)
 dat2 <- runif(1000)
 dat3 <- runif(1000)
 
-# Shapiro-Wilk test for normality
+### Shapiro-Wilk test for normality
 shapiro.test(dat1)
 
-# Anderson-Darling test for normality
+### Anderson-Darling test for normality
 ad.test(dat1)
 
-# Student's t-test
+### Student's t-test
 t.test(dat1, dat2)
 
-# Kendall's Tau
+### Kendall's Tau
 cor.test(dat1, dat2, method="kendall")
 
-# ANOVA
+### ANOVA
 df <- reshape2::melt(cbind(dat1, dat2, dat3))[,c("Var2","value")]
 colnames(df) <- c("group","value")
 anovaOut <- aov(value ~ group, data=df)
 summary(anovaOut)
 
-# Pearson's Chi-Squared
+### Pearson's Chi-Squared
 tbl <- table(survey$Smoke, survey$Exer) 
 chisq.test(tbl) 
+# correction for 2x2 contingency table, ignored if input > 2x2
+tbl2x2 <- tbl[c(1:2),c(1:2)]
+chisq.test(tbl2x2, correct=TRUE)
 
-# Mann-Whitney U test
+
+### Mann-Whitney U test
 wilcox.test(dat1, dat2)
 
-# test for proportions
+### test for proportions
 prop.test(cbind(dat1, dat2), alternative="two.sided")
 
-# Augmented Dickey-Fuller test for time-series autoregressiveness
+### Augmented Dickey-Fuller test for time-series autoregressiveness
 adf.test(dat1)
