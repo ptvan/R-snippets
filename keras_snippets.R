d728cf39eb291f99fc3875377297b9b3a09a2b8b
ptvan <pvan@fredhutch.org>
2020-07-15 07:46:16 -0700

    added custom models

--- keras_snippets.R
+++ keras_snippets.R
@@ -1,6 +1,7 @@
 library(keras)
 
-### MNIST
+### MNIST classification
+# load data
 mnist <- dataset_mnist()
 
 x_train <- mnist$train$x
@@ -58,3 +59,95 @@ x <- array_reshape(x, c(1, dim(x)))
 x <- imagenet_preprocess_input(x)
 
 features <- model %>% predict
+
+### Generative Adversarial Network
+
+# generator custom model
+generator <-
+  function(name = NULL) {
+    keras_model_custom(name = name, function(self) {
+      
+      self$fc1 <- layer_dense(units = 7 * 7 * 64, use_bias = FALSE)
+      self$batchnorm1 <- layer_batch_normalization()
+      self$leaky_relu1 <- layer_activation_leaky_relu()
+      self$conv1 <-
+        layer_conv_2d_transpose(
+          filters = 64,
+          kernel_size = c(5, 5),
+          strides = c(1, 1),
+          padding = "same",
+          use_bias = FALSE
+        )
+      self$batchnorm2 <- layer_batch_normalization()
+      self$leaky_relu2 <- layer_activation_leaky_relu()
+      self$conv2 <-
+        layer_conv_2d_transpose(
+          filters = 32,
+          kernel_size = c(5, 5),
+          strides = c(2, 2),
+          padding = "same",
+          use_bias = FALSE
+        )
+      self$batchnorm3 <- layer_batch_normalization()
+      self$leaky_relu3 <- layer_activation_leaky_relu()
+      self$conv3 <-
+        layer_conv_2d_transpose(
+          filters = 1,
+          kernel_size = c(5, 5),
+          strides = c(2, 2),
+          padding = "same",
+          use_bias = FALSE,
+          activation = "tanh"
+        )
+      
+      function(inputs, mask = NULL, training = TRUE) {
+        self$fc1(inputs) %>%
+          self$batchnorm1(training = training) %>%
+          self$leaky_relu1() %>%
+          k_reshape(shape = c(-1, 7, 7, 64)) %>%
+          self$conv1() %>%
+          self$batchnorm2(training = training) %>%
+          self$leaky_relu2() %>%
+          self$conv2() %>%
+          self$batchnorm3(training = training) %>%
+          self$leaky_relu3() %>%
+          self$conv3()
+      }
+    })
+  }
+
+# discriminator custom model
+discriminator <-
+  function(name = NULL) {
+    keras_model_custom(name = name, function(self) {
+      
+      self$conv1 <- layer_conv_2d(
+        filters = 64,
+        kernel_size = c(5, 5),
+        strides = c(2, 2),
+        padding = "same"
+      )
+      self$leaky_relu1 <- layer_activation_leaky_relu()
+      self$dropout <- layer_dropout(rate = 0.3)
+      self$conv2 <-
+        layer_conv_2d(
+          filters = 128,
+          kernel_size = c(5, 5),
+          strides = c(2, 2),
+          padding = "same"
+        )
+      self$leaky_relu2 <- layer_activation_leaky_relu()
+      self$flatten <- layer_flatten()
+      self$fc1 <- layer_dense(units = 1)
+      
+      function(inputs, mask = NULL, training = TRUE) {
+        inputs %>% self$conv1() %>%
+          self$leaky_relu1() %>%
+          self$dropout(training = training) %>%
+          self$conv2() %>%
+          self$leaky_relu2() %>%
+          self$flatten() %>%
+          self$fc1()
+      }
+    })
+  }
