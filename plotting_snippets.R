f493ccceb64d98dfa320119e6c3866d4038c2dd1
ptvan <pvan@fredhutch.org>
2020-09-11 08:54:20 -0700

    added ggridges

--- plotting_snippets.R
+++ plotting_snippets.R
@@ -1,3 +1,14 @@
 library(viridis)
 library(ComplexHeatmap)
 library(cowplot)
+library(ggridges)
+
+# viridis provides colorblindness-safe palettes 
+
+
+# ComplexHeatmap allows partitioning and more robust annotation
+
+
+# ggridges (formerly ggjoy) plots ridgelines
+
+
