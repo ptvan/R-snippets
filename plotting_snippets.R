ce1c668ae775cc854faa8e62958b5894814e7fba
ptvan <pvan@fredhutch.org>
2020-12-14 07:12:35 -0800

    added ComplexHeatmap

--- plotting_snippets.R
+++ plotting_snippets.R
@@ -34,9 +34,20 @@ stat_compare_means(comparisons=list(c("0.5","1"), c("1","2"), c("0.5","2"))
                    ,label="p.signif"
                    ) 
 
-
 # ComplexHeatmap allows partitioning and more robust annotation
+nr1 <- 4; nr2 <- 8; nr3 <- 6; nr <- nr1 + nr2 + nr3
+nc1 <- 6; nc2 <- 8; nc3 <- 10; nc <- nc1 + nc2 + nc3
 
+mat <- cbind(rbind(matrix(rnorm(nr1*nc1, mean = 1,   sd = 0.5), nr = nr1),
+                  matrix(rnorm(nr2*nc1, mean = 0,   sd = 0.5), nr = nr2),
+                  matrix(rnorm(nr3*nc1, mean = 0,   sd = 0.5), nr = nr3)),
+            rbind(matrix(rnorm(nr1*nc2, mean = 0,   sd = 0.5), nr = nr1),
+                  matrix(rnorm(nr2*nc2, mean = 1,   sd = 0.5), nr = nr2),
+                  matrix(rnorm(nr3*nc2, mean = 0,   sd = 0.5), nr = nr3)),
+            rbind(matrix(rnorm(nr1*nc3, mean = 0.5, sd = 0.5), nr = nr1),
+                  matrix(rnorm(nr2*nc3, mean = 0.5, sd = 0.5), nr = nr2),
+                  matrix(rnorm(nr3*nc3, mean = 1,   sd = 0.5), nr = nr3))
+)
 
 # ggridges (formerly ggjoy) plots ridgelines
 
