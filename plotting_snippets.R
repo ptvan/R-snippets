dc2757864607296cae003a55489b2652c1d27d02
ptvan <nulzilla@gmail.com>
2021-11-09 09:36:22 -0800

    added ComplexHeatmap margins

--- plotting_snippets.R
+++ plotting_snippets.R
@@ -1,11 +1,11 @@
-# viridis provides colorblindness-safe palettes 
+#### viridis provides colorblindness-safe palettes 
 library(viridis)
 ggplot(mtcars, aes(wt, mpg)) + 
   geom_point(size=4, aes(colour = factor(cyl))) +
   scale_color_viridis(discrete=TRUE) +
   theme_bw()
 
-# ggpubr provides p-values for comparing groups with ggboxplot
+#### ggpubr provides p-values for comparing groups with ggboxplot
 library(ggpubr)
 ggboxplot(ToothGrowth, 
           x="dose", 
@@ -29,7 +29,7 @@ stat_compare_means(comparisons=list(c("0.5","1"), c("1","2"), c("0.5","2"))
                    ,label="p.signif"
                    ) 
 
-# ComplexHeatmap allows partitioning and more robust annotation
+#### ComplexHeatmap allows partitioning and more robust annotation
 library(ComplexHeatmap)
 nr1 <- 4; nr2 <- 8; nr3 <- 6; nr <- nr1 + nr2 + nr3
 nc1 <- 6; nc2 <- 8; nc3 <- 10; nc <- nc1 + nc2 + nc3
@@ -47,15 +47,18 @@ mat <- cbind(rbind(matrix(rnorm(nr1*nc1, mean = 1,   sd = 0.5), nr = nr1),
 mat <- mat[sample(nr, nr), sample(nc, nc)]
 rownames(mat) <- paste0("row", seq_len(nr))
 colnames(mat) <- paste0("column", seq_len(nc))
+g <- Heatmap(mat, name = "mat", row_km = 2)
 
-Heatmap(mat, name = "mat", row_km = 2)
+# adjust page margins (bottom, left, top, right)
+draw(g, unit(c(1.0, 0.5, 0.5, 1.0), "in" ))
 
-# ggridges (formerly ggjoy) plots ridgelines
+
+#### ggridges (formerly ggjoy) plots ridgelines
 library(ggridges)
 ggplot(iris, aes(x = Sepal.Length, y = Species)) + 
   geom_density_ridges(scale = 0.9)
 
-# ggsurvplot from survminer, support risk tables, confidence intervals and p-vals
+#### ggsurvplot from survminer, support risk tables, confidence intervals and p-vals
 library(survminer)
 fit <- survfit(Surv(time, status) ~ sex, data = lung)
 
@@ -70,12 +73,12 @@ ggsurvplot(fit,
 )
 
 
-# venn supports up to 7-set Venn diagrams
+#### venn supports up to 7-set Venn diagrams
 library(venn)
 x <- as.data.frame(matrix(sample(0:1, 150, replace = TRUE), ncol = 5))
 venn(x, snames = "A, B, C, D, E", zcolor = "red, blue, green, yellow, purple")
 
-# gghighlight
+#### gghighlight
 library(gghighlight)
 
 ggplot(d) +
