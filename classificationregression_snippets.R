fca077a9ae27ffc90728bf03ba3477540081a509
ptvan <nulzilla@gmail.com>
2023-07-06 14:34:39 -0700

    cleaned up xgboost

--- classificationregression_snippets.R
+++ classificationregression_snippets.R
@@ -122,8 +122,8 @@ sum(diag(training_table))/sum(training_table)
 ##########################
 library(xgboost)
 
-data(agaricus.train, package='xgboost')
-data(agaricus.test, package='xgboost')
+data(agaricus.train, package = 'xgboost')
+data(agaricus.test, package = 'xgboost')
 train <- agaricus.train
 test <- agaricus.test
 
@@ -133,28 +133,22 @@ bst_rf <- xgboost(data = train$data
                   , max_depth = 4
                   , num_parallel_tree = 1000
                   , subsample = 0.5
-                  , colsample_bytree =0.5
+                  , colsample_bytree = 0.5
                   , nrounds = 1
                   , objective = "binary:logistic")
 
-bst <- xgboost(data = dtrain
-               , booster = "gbtree"
-               , max_depth = 2
-               , eta = 1
-               , nthread = 2
-               , nrounds = 2
-               , objective = "binary:logistic", verbose = 2)
-
 # XGB's default is regression, so the predictions are probabilities
-pred <- predict(bst, test$data)
+pred <- predict(bst_rf, test$data)
 
 prediction <- as.numeric(pred > 0.5)
 err <- mean(prediction != test$label)
 print(paste("test-error=", err))
 
-xgb.importance(model = bst)
-xgb.plot.shap(agaricus.test$data, model = bst, features = "odor=none")
+# variable importance
+xgb.importance(model = bst_rf)
 
+# SHAP plot
+xgb.plot.shap(agaricus.test$data, model = bst_rf, features = "odor=none")
 
 #########################
 # Support Vector Machines
